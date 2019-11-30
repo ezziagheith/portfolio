4249b109e6459f34e759172d7e65a661df20baae
ezziagheith <ezziagheith@gmail.com>
2019-11-30 12:33:29 -0800

    Contact me page

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 $(function() {
-    $( ".jigsaw1,.jigsaw2,.jigsaw3,.jigsaw4,.jigsaw5,.jigsaw6,.jigsaw7" ).draggable();
+    $( ".jigsaw1,.jigsaw2,.jigsaw3,.jigsaw4,.jigsaw5,.jigsaw6,.jigsaw7,.jigsaw8" ).draggable();
   });
 
 
